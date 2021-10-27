f1b12b03b069ecca09d1b70f7d9f79b4901ef596
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-10-28 01:39:38 +0200

    push new code

--- C/cards.c
+++ C/cards.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <stdio.h>
+#include <stdlib.h>
 int main()
 {
     char card_name[3];
     puts("Enter the card_name: ");
-    scanf("2%s", card_name);
-
-    int val=0;
-
-    if(card_name[0]=='K'){
-        val =10;
-    }else if(card_name[0]=='Q'){
-        val =10;
-    }else if(card_name[0]=='J'){
-        val=10;
-    }else if(card_name[0] == 'A'){
+    scanf("%2s", card_name);
+    int val = 0;
+    if (card_name[0] == 'K') {
+        val = 10;
+    } else if (card_name[0] == 'Q') {
+        val = 10;
+    } else if (card_name[0] == 'J') {
+        val = 10;
+    } else if (card_name[0] == 'A') {
         val = 11;
-    }else{
-        val = atoi(card_name); //The "atoi()" function converts a text(the argument) to a number. 
+    } else {
+        val = atoi(card_name);
     }
     printf("The card value is: %i\n", val);
-
-
-
+    return 0;
 }
---
+++ C/cards1.c
@@ -0,0 +1,34 @@
+//we are going to add a checker to the code
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    char card_name[3];
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+
+    int val=0;
+
+    if(card_name[0]=='K'){
+        val =10;
+    }else if(card_name[0]=='Q'){
+        val =10;
+    }else if(card_name[0]=='J'){
+        val=10;
+    }else if(card_name[0] == 'A'){
+        val = 11;
+    }else{
+        val = atoi(card_name); //The "atoi()" function converts a text(the argument) to a number. 
+    }
+    printf("The card value is: %i\n", val);
+
+    if ((val>=3)&&(val<=6)){
+        puts("Count has gone up");
+    }
+    else if (val==10){
+        puts("Count has gone down.");
+    }
+
+}
