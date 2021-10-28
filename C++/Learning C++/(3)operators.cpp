ea14c70bb83fa8604f720b5778e243e9932039db
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-10-28 02:48:27 +0200

    new update

---
+++ C++/Learning C++/(3)operators.cpp
@@ -0,0 +1,20 @@
+/*Bitwise boolean operators (binary opeartions performed bit by bit between the operand )
+ & = bitwise AND
+ | = bitwise OR
+ ~ = NOT
+ ^ = XOR
+it shoud be noted this are bitwise operation not logical
+*/
+
+/*
+Logical operations are the ones you use with if statements
+&& = AND
+|| = OR
+! = NOT
+*/
+
+/*
+* = Gives you acces to the variable the pointer is pointing to (defrences the pointer to it's right)
+& = "prefix &"" returns the address of the variable at it's right
+-> = Gives you access to members of classes or structures from a pointer
+*/
