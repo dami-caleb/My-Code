024e8377945a4a1f18f98831e60a34250919945b
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-01 13:42:05 +0100

    Update type_inference.cpp

--- C++/Learning C++/type_inference.cpp
+++ C++/Learning C++/type_inference.cpp
@@ -1,9 +1,19 @@
- #include <iostream>
+/*
+#include <iostream>
 #include <typeinfo>
 
 using namespace std;
+
+auto a =8;
+auto b =1223344555444;
+auto c = 'd'
+
+//including the typeinfo header file. The "auto" automatically determines the data type for us.
+//one thing is you have to "initialize" the variable at decleartion
+
 
 int main(){
 
     return 0;
 }
+*/
