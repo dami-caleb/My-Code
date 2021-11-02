a1d1d12eb9811798fa6cb8be91ffe8d24112c6e1
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-02 19:02:51 +0100

    Create strings.cpp

---
+++ C++/Learning C++/strings.cpp
@@ -0,0 +1,7 @@
+//strings are not a native datatype in the c++ language
+/*Just like in the C programming language you can represent STRINGS
+as an array of characters*/
+
+/*You can also use the C++ string class
+The std::string class comes with the C++ standard library
+*/
