4250aea55301e025a96e2e2c16498d0fdfe2cddf
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-02 19:04:29 +0100

    Update strings.cpp

--- C++/Learning C++/strings.cpp
+++ C++/Learning C++/strings.cpp
@@ -5,3 +5,13 @@ as an array of characters*/
 /*You can also use the C++ string class
 The std::string class comes with the C++ standard library
 */
+
+#include <iostream>
+#include <string>  //this string header contains the string class
+using namespace std;
+
+int main(){
+
+
+return 0;
+}
