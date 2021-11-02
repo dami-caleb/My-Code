fee1c585fef61329aa2fd946cad8d8a66e9602cd
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-02 18:12:01 +0100

    Update preprocessor_directives.cpp

---
+++ C++/Learning C++/preprocessor_directives.cpp
@@ -0,0 +1,6 @@
+//Your code is preprocessed prior to compiling
+//There are several directives addressed to the preprocessor 
+//which contain information about what to add or remove in the code
+//The compiler recives a modeified version of the code from the preprocessor
+//Directives tell the preprocessor what to add or remove in the code
+//Useful for libraries, constants, selecting code blocks, etc.
