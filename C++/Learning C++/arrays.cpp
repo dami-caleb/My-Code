6abb66d0e83298753c63cbdb4fcc96cfdcd88342
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-02 18:36:31 +0100

    Create arrays.cpp

---
+++ C++/Learning C++/arrays.cpp
@@ -0,0 +1,15 @@
+//Arrays are collection of data
+
+#include <iostream>
+using namespace std;
+
+int four[4];
+int main(){
+    four[0]=4;
+    four[1]=8;
+    four[2]=16;
+    four[3]=420
+
+    cout<<"4*1 = "<<
+
+}
