7ee47c786d7100c11d0bbb8545c6e164c4b37dac
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-02 18:40:25 +0100

    Update arrays.cpp

--- C++/Learning C++/arrays.cpp
+++ C++/Learning C++/arrays.cpp
@@ -8,8 +8,14 @@ int main(){
     four[0]=4;
     four[1]=8;
     four[2]=16;
-    four[3]=420
+    four[3]=420;
 
-    cout<<"4*1 = "<<
+    cout <<"4*1 = "<<four[0]<<endl;
+    cout <<"4*2 = "<< four[1]<<endl;
+    cout <<"4*3 = "<<four[2] <<endl;
+    cout <<"4*4 = "<<four[3] <<endl;
+
+
+
 
 }
