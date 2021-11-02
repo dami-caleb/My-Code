893421e7422c27f09000c215c4e1b8ed357e8849
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-02 18:17:00 +0100

    Update preprocessor_directives.cpp

--- C++/Learning C++/preprocessor_directives.cpp
+++ C++/Learning C++/preprocessor_directives.cpp
@@ -4,3 +4,10 @@
 //The compiler recives a modeified version of the code from the preprocessor
 //Directives tell the preprocessor what to add or remove in the code
 //Useful for libraries, constants, selecting code blocks, etc.
+
+/*All directives start with the "#" charcter*/
+
+//example
+ #include <iostream> //The "#inculde directive here replaces all the line "e.g. #include <stdlib.h> with the file itself (Yes! all that files is represented with just the #include directive)" 
+ //Notice that iostream does not have a path or extension. Also notice that it is enclosed in angled brackets. That means that the file will be searched in a predefined location, usually 
+ //specified by the development environment, like a default include folder. 
