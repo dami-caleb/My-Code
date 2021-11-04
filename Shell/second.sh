d2b061b419eec57eef3b8484ed4f6ca8ec887e75
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-04 21:47:00 +0100

    push new update

--- Shell/second.sh
+++ Shell/second.sh
@@ -2,7 +2,7 @@
 echo -e "Welcome to the math examiner.\n What is your name?"  #the "-e" makes it possible for us to use the new line charcter
 read user_name
 read -p "By solving the equation a = 20 - 9a, the value of a will be: " user_solution
-computer_solution = 2
+int computer_solution = 2
 if [$user_soution == $computer_solution]
 then
 	echo"Nice Job!"
