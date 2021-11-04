25075385e44bc230ff0cac29832f1cb7cb31dcbb
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-04 21:09:06 +0100

    shell script
    This shell script tells the computer to do some task

---
+++ Shell/myfirst.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+clear
+echo "I love JESUS and the HOLYSPIRIT. I AM MOVING WITH THE SPEED OF THE HOLYGHOST!"
+echo "What is your name? "
+read x
+
+read -p "Hello $x! how old are you?: "  y
+echo "Wow I am also "$y" years(s) old! "
+
+echo "It was nice chatting with you $x. But I got to gooooooooo"
