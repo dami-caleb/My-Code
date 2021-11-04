7f952014caff794de505fce9488de1060a95e955
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-04 21:49:28 +0100

    push new update

--- Shell/second.sh
+++ Shell/second.sh
@@ -1,5 +1,5 @@
 
-echo -e "Welcome to the math examiner.\n What is your name?"  #the "-e" makes it possible for us to use the new line charcter
+echo  "Welcome to the math examiner.\n What is your name?"  #the "-e" makes it possible for us to use the new line charcter
 read user_name
 read -p "By solving the equation a = 20 - 9a, the value of a will be: " user_solution
 int computer_solution = 2
