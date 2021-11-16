49111de65113cd219d1054c9ca6cfd97a4523f71
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-16 19:23:16 +0100

    Update strings.cpp

--- C++/Learning C++/strings.cpp
+++ C++/Learning C++/strings.cpp
@@ -8,10 +8,14 @@ The std::string class comes with the C++ standard library
 
 #include <iostream>
 #include <string>  //this string header contains the string class
-#include <cstring>
+#include <cstring> //this contains functions to handle the charcter array strings (the c infornt means it is part of the c standard library)
 using namespace std;
 
+cp
+
 int main(){
+
+char name[] = "Jenifer";
 
 
 return 0;
