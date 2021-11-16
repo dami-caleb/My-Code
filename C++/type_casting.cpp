1328943ab6ee45fb40b66b89c70948b1a85860d9
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-11-16 20:08:57 +0100

    type casting

---
+++ C++/type_casting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    
+    int fahrenheit = 100;
+    int celsius;
+
+    celsius = ((float)5/9)*(fahrenheit -32 );
+    cout<<celsius<<endl;
+
+    return 0;
+}
