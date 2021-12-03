7ed44397ea2fc8f3460814c29541c8f5b93e3134
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-12-03 16:14:59 +0100

    Update 3.user_input_with_split.py

--- Python/caleb c/Part_1_2/(6)User_data/3.user_input_with_split.py
+++ Python/caleb c/Part_1_2/(6)User_data/3.user_input_with_split.py
@@ -1,6 +1,8 @@
-data= input("Sup tell me your favourite movies (separate them by spaces): ")
+#data= input("Sup tell me your favourite movies (separate them by spaces): ")
 
-lists = data.split()
+#lists = data.split()
 
-for movie in lists:
-    print(lists)
+#for movie in lists:
+#    print("Okay so your favourite movies are", lists)
+
+data = input("How are you?")
