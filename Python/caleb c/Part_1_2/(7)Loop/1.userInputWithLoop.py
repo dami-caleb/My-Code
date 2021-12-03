8499ea33ff2e491967f58a99182edc832bf9758d
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-12-03 16:55:32 +0100

    Update 1.userInputWithLoop.py

--- Python/caleb c/Part_1_2/(7)Loop/1.userInputWithLoop.py
+++ Python/caleb c/Part_1_2/(7)Loop/1.userInputWithLoop.py
@@ -1,10 +1,14 @@
-print("What are your favourite fruits?")
+print("What are your favourite fruits? Press enter after each fruit")
 print("Enter 'q' to quit: ")
 
 fruits = []
 
 while True:
     data = input()
+    fruits.append(data)
     if str.lower(data) =='q':
         break
-    fruits.append(data)
+    
+    
+for fruit in fruits:
+    print("one of your favourite fruit is: ",fruit)
