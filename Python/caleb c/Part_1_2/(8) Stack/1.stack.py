ae87dcf6addc4fe49980638611352d7a89373304
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-12-03 17:04:19 +0100

    Create 1.stack.py

---
+++ Python/caleb c/Part_1_2/(8) Stack/1.stack.py
@@ -0,0 +1,8 @@
+#data structure is a way to structure data (eg. list,set, distionary e.t.c)
+#another one this  ways is a stack 
+
+stack = []
+stack.append("item 1")
+stack.append("item 2")
+print(stack.pop())  #the latest item added gets removed first just like a stack of books, to remove an item from 
+                    #you remove from the top before the bottom
