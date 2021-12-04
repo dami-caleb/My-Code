bdad9e416b06e469153a0f53277d7a566eeacd26
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-12-04 05:56:02 +0100

    Update 1.dictionaries.py

---
+++ Python/caleb c/Part_1_2/(14)Dictionaries/1.dictionaries.py
@@ -0,0 +1,29 @@
+#Dictionaries are data structure to hold numerous elements
+#The difference is in dictionaries there is an accociation of two elements
+
+emails = {
+    "james":"james@email.com",   #The name on the left (james) is called the key
+    "jane":"jane@email.com"     #The email on the right is called the value 
+}
+
+number = {
+    "james": 1323445555,
+    "jane": 3244555555
+}
+
+print(emails)
+print(number)
+
+
+#The key has to be a value that is hashable
+#hashable means the data can be converetd to a number
+
+Pass = {"James":True, "Jane":False}
+print(Pass)
+
+print(hash(True))  ##see true can be hashable
+
+#dictionaries don't work with mutable types 
+#A tuple is like a string, but it is not mutable
+
+#mutable =  the data in can be changed
