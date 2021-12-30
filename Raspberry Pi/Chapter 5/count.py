1fc0120bd2132e4167b25fa60084051e3931f8fd
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-12-30 19:05:53 +0100

    Update count.py
    An application has been created

--- Raspberry Pi/Chapter 5/count.py
+++ Raspberry Pi/Chapter 5/count.py
@@ -1,2 +1,21 @@
-for i in range(10):
-    print (i)
+mangoes = 10
+
+print("Welcome to the mango dispenser.")
+Welcome_screen = "How many magoes do you want?"
+print(Welcome_screen)
+
+user_input = int(input("Enter number: "))
+
+if (user_input>mangoes):
+    print("The number of mangoes you want is more than the number of mangoes we have")
+    too_much_screen = print("Do you want to see the number of mangoes you can take? ")
+    user_input1 = input("Enter Yes or No: ").lower()
+
+    if(user_input1=="yes"):
+        for mango in range(mangoes):
+            print ("You can take",mango,"mangoes")
+    else:
+        print("Goodbye!")
+else:
+    remaining = mangoes - user_input
+    print("You have ", remaining, "mangoes remaining")
