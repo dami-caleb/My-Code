17ad0d06f6890f5e573eebf89fad22a7b10719da
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-12-30 21:25:18 +0100

    Update temprature.py

--- Raspberry Pi/Chapter 5/temprature.py
+++ Raspberry Pi/Chapter 5/temprature.py
@@ -1,21 +1,37 @@
 
-class converter():
-    def Celsius_to_Fahrenheit():
-        tempC = int(input("Enter the Celsius temprature you want to conver to Fahrenheit: "))
-        tempF = ((tempC)*9)/5 +32
-        print("The temprature in Fahrenheit is ", tempF)
+
+def Celsius_to_Fahrenheit():
+    tempC = int(input("Enter the Celsius temprature you want to conver to Fahrenheit: "))
+    tempF = ((tempC)*9)/5 +32
+    print("The temprature in Fahrenheit is ", tempF)
+
+#Celsius_to_Fahrenheit()
+
+def Binary_to_Decimal():
+    binary_value = input("Enter the binary value you wish to convert to decimal: ")
+    decimal_value = int(binary_value,2)
+    print("The decimal equivalent of ", binary_value, "is ", decimal_value)
+
+#Binary_to_Decimal()
+
+def Hex_to_Dec():
+    hex_value = input("Enter the hexadecimal value you wish to convert to decimal: ")
+    decimal_value = int(hex_value,16)
+    print("The decimal equivalent of ", hex_value, "is ", decimal_value)
 
 
-    def Binary_to_Decimal():
-        binary_value = input("Enter the binary value you wish to convert to decimal: ")
-        decimal_value = int(binary_value,2)
-        print("The decimal equivalent of ", binary_value, "is ", decimal_value)
+#You need to find the position of one string within another.
+#Use the find Python function.
+
+#For example, to find the starting 
+# position of the string "wwe" within 
+# the string frfwwevvvv, you would use:
+
+s = "frfwwevvvv"
+print(s.find("wwe"))
 
 
-    def Hex_to_Dec():
-        hex_value = input("Enter the hexadecimal value you wish to convert to decimal: ")
-        decimal_value = int(hex_value,16)
-        print("The decimal equivalent of ", hex_value, "is ", decimal_value)
+
 
 
 
