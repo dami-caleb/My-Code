1566f88a9f915811dac864016e23ed4e74cfc4ef
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-12-30 20:39:57 +0100

    Update temprature.py
    All the functions have been kept into one class

--- Raspberry Pi/Chapter 5/temprature.py
+++ Raspberry Pi/Chapter 5/temprature.py
@@ -1,22 +1,21 @@
 
-def Celsius_to_Fahrenheit():
-    tempC = int(input("Enter the Celsius temprature you want to conver to Fahrenheit: "))
-    tempF = ((tempC)*9)/5 +32
-    print("The temprature in Fahrenheit is ", tempF)
+class converter():
+    def Celsius_to_Fahrenheit():
+        tempC = int(input("Enter the Celsius temprature you want to conver to Fahrenheit: "))
+        tempF = ((tempC)*9)/5 +32
+        print("The temprature in Fahrenheit is ", tempF)
 
-Celsius_to_Fahrenheit()
 
-def Binary_to_Decimal():
-    binary_value = input("Enter the binary value you wish to convert to decimal: ")
-    decimal_value = int(binary_value,2)
-    print("The decimal equivalent of ", binary_value, "is ", decimal_value)
+    def Binary_to_Decimal():
+        binary_value = input("Enter the binary value you wish to convert to decimal: ")
+        decimal_value = int(binary_value,2)
+        print("The decimal equivalent of ", binary_value, "is ", decimal_value)
 
-Binary_to_Decimal()
 
-def Hex_to_Dec():
-    hex_value = input("Enter the hexadecimal value you wish to convert to decimal: ")
-    decimal_value = int(hex_value,16)
-    print("The decimal equivalent of ", hex_value, "is ", decimal_value)
+    def Hex_to_Dec():
+        hex_value = input("Enter the hexadecimal value you wish to convert to decimal: ")
+        decimal_value = int(hex_value,16)
+        print("The decimal equivalent of ", hex_value, "is ", decimal_value)
 
 
 
