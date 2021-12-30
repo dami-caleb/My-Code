875e4f781f94a658cfb730be6d996a333ba4c56f
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2021-12-30 20:33:27 +0100

    Update temprature.py
    An extra function to convert binary to has been added to source code

---
+++ Raspberry Pi/Chapter 5/temprature.py
@@ -0,0 +1,17 @@
+def Celsius_to_Fahrenheit():
+    tempC = int(input("Enter the Celsius temprature you want to conver to Fahrenheit: "))
+    tempF = ((tempC)*9)/5 +32
+    print("The temprature in Fahrenheit is ", tempF)
+
+Celsius_to_Fahrenheit()
+
+def Binary_to_Decimal():
+    binary_value = input("Enter the binary value you wish to convert to decimal: ")
+    decimal_value = int(binary_value,2)
+    print("The decimal equivalent of ", binary_value, "is ", decimal_value)
+
+Binary_to_Decimal()
+
+
+#print(f"""crrf/|+
+#\gf%""")
