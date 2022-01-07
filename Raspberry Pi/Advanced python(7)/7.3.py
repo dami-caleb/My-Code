91b8b2dcee4de97ea0a98bb8d8625f0dd1f35053
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2022-01-07 18:32:54 +0100

    Create 7.3.py

---
+++ Raspberry Pi/Advanced python(7)/7.3.py
@@ -0,0 +1,16 @@
+#returning ,ore than one value
+
+#You need to write a function that returns more than one value.
+#Solution:
+#You can return a tuple.
+#A tuple is a Python data structure
+#that’s a little like a list, except that tuples are enclosed in parentheses
+#rather than brackets.
+
+def calculate_tempratures(kelvin):
+    celsius = kelvin-273
+    fahrenheit = celsius *9/5 + 32
+    return celsius, fahrenheit
+
+celsius_value, fahrenheit_value = calculate_tempratures(42)
+print(celsius_value)
