90c3f9123d1c0b09ccd1a0def5b171bd265c87c7
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2022-01-18 19:05:30 +0100

    Update 7.12.py
    Add processing time

--- Raspberry Pi/Advanced python(7)/7.12.py
+++ Raspberry Pi/Advanced python(7)/7.12.py
@@ -2,6 +2,7 @@
 #data entry
 
 import re
+import time
 
 username_regex = '^[‘\w_\.+-’]+@[\w_\.-]+\.[\w_-]+$'
 password_regex = '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$'  #requires at least one special character
@@ -13,6 +14,7 @@ while wrong_login_details:
     user_email, user_password = input("Kindly enter your email address to login: "), input("Enter your password: ")
 
     print("Processing......")
+    time.sleep(2)
     if re.search(username_regex, user_email) and re.search(password_regex,user_password):
         print("Your portal is loading.")
         wrong_login_detailscondition = False
