1ca678589969039b0d952c86575a31d2204f2747
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2022-01-18 18:46:24 +0100

    Add source code for password

--- Raspberry Pi/Advanced python(7)/7.12.py
+++ Raspberry Pi/Advanced python(7)/7.12.py
@@ -3,12 +3,14 @@
 
 import re
 
-regex = '^[‘\w_\.+-’]+@[\w_\.-]+\.[\w_-]+$'
+username_regex = '^[‘\w_\.+-’]+@[\w_\.-]+\.[\w_-]+$'
+password_regex = '[*.!@#$%^&(){}[]:;<>,.?/~_+-=|\]'  #requires at least one special character
 
 print("Welcome to the emf portal.")
-while True:
-    user_email = input("Kindly enter your email address to login: ")
-    if re.search(regex, user_email):
-        print("Your portal is loading.")
-    else:
-        print("Wrong email address, please try again.")
+user_email, user_password = input("Kindly enter your email address to login: "), input("Enter your password: ")
+
+print("Processing......")
+if re.search(username_regex, user_email) and re.search(password_regex,user_password):
+    print("Your portal is loading.")
+else:
+    print("Wrong email or password entered, please try again.")
