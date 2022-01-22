9e9be816d445c4096425436a9ea1656e23750cac
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2022-01-22 01:21:11 +0100

    add image to be edited

--- Guizero/3.Wanted_poster.py
+++ Guizero/3.Wanted_poster.py
@@ -3,6 +3,13 @@ from guizero import App, Text
 app = App("Wanted!")
 
 app.bg = (245, 197,186) #The arguments are the RGB values and "bg" stands for background
-wanted_message = Text(app,"WANTED!!!!! DEAD OR ALIVE!")
+
+wanted_message = Text(app,"WANTED!!!!!")
+wanted_message.font = "Times New Roman"
+wanted_message.text_size = 30
+
+wanted_message1 = Text(app,"DEAD OR ALIVE.")
+wanted_message1.font = "Times New Roman"
+wanted_message1.text_size = 20
 
 app.display()
