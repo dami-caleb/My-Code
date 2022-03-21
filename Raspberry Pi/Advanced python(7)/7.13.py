509a5061b24bf8e15290aba22d46bdb3f8fac254
dami-caleb <72282670+dami-caleb@users.noreply.github.com>
2022-03-21 22:59:14 +0100

    Delete Raspberry Pi directory

--- Raspberry Pi/Advanced python(7)/7.13.py
+++
@@ -1,9 +0,0 @@
-import re
-import urllib.request
-
-regular_expression = '(#[/d,]+) in Books'
-url = 'https://www.amazon.com/Raspberry-Pi-Cookbook-Software-Solutions/dp/1492043222/'
-
-print("The Amazon rank of Raspberry pi cookbook is: ")
-text = urllib.request.urlopen(url).read().decode('utf-8')
-print(re.search(regular_expression,text).group())
